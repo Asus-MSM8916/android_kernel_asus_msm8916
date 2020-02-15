b104711c6c7a260b5f451b0c26c6ea2f281c2a41
Roker2 <dimaminko2000@gmail.com>
2020-02-15 14:53:56 +0300

    drivers: net: wireless: Add additional name of NVBIN_FILE for ZE500KL

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -258,7 +258,11 @@ static struct notifier_block wnb = {
 	.notifier_call = wcnss_notif_cb,
 };
 
+#ifdef CONFIG_ZE500KL_NVBIN_FILE
+#define NVBIN_FILE "wlan/prima/WCNSS_qcom_wlan_nv_ze500kl.bin"
+#else
 #define NVBIN_FILE "wlan/prima/WCNSS_qcom_wlan_nv.bin"
+#endif
 
 /* On SMD channel 4K of maximum data can be transferred, including message
  * header, so NV fragment size as next multiple of 1Kb is 3Kb.
